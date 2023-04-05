524f14939ae4591f73e6ebf1bdeaee01226d4ec3
yeongjun Kim <ehdwjs500dnj@naver.com>
2023-04-06 05:03:42 +0900

    refactor: 컴포넌트 모듈화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,32 @@
+import FloatingButton from "@/Components/floating-button";
+import Item from "@/Components/item";
+import Layout from "@/Components/layout";
+
 export default function Home() {
-  return null;
+  return (
+    <Layout title="홈" hasTabBar>
+      <div className="flex flex-col  space-y-5 divide-y">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item key={i} id={i} title="iphone 14" price={99} hearts={1} comments={1} />
+        ))}
+        <FloatingButton href="/items/upload">
+          <svg
+            className="h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg>
+        </FloatingButton>
+      </div>
+    </Layout>
+  );
 }
