f094538086a3b6231d63c329304d0f3786808abe
yeongjun Kim <ehdwjs500dnj@naver.com>
2023-04-05 18:53:07 +0900

    feat: 프로필 페이지 추가

---
+++ src/pages/profile/bought.tsx
@@ -0,0 +1,14 @@
+import Item from "@/Components/item";
+import Layout from "@/Components/layout";
+
+export default function Bought() {
+  return (
+    <Layout canGoBack>
+      <div className="flex flex-col space-y-5 pb-10 divide-y">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item key={i} id={i} title="iphone 14" price={99} hearts={1} comments={1} />
+        ))}
+      </div>
+    </Layout>
+  );
+}
