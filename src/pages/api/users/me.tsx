ca59d02728dd4d1374d828dca27736f093751ca3
yeongjun Kim <kyjun519@gmail.com>
2023-04-09 22:09:02 +0900

    feat: 유저 정보를 얻어오는 api 생성

---
+++ src/pages/api/users/me.tsx
@@ -0,0 +1,20 @@
+import client from "@/libs/server/client";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const profile = await client.user.findUnique({
+    where: {
+      id: req.session.user?.id,
+    },
+  });
+  res.json({
+    ok: true,
+    profile,
+  });
+}
+
+export default withApiSession(withHandler("GET", handler));
+
+// withIronSessionApiRoute로 감싸면 req.session을 확인할 수 있다.
