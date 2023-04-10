787b8e87bd87b88cfd75685761b19b5ce74a6b69
yeongjun Kim <kyjun519@gmail.com>
2023-04-10 22:45:11 +0900

    feat: SWR global config 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr"; //모든 SWR 훅에 대한 global 옵션을 제공
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="w-full max-w-xl mx-auto">
-      <Component {...pageProps} />
-    </div>
+    <SWRConfig value={{ fetcher: (url: string) => fetch(url).then((response) => response.json()) }}>
+      <div className="w-full max-w-xl mx-auto">
+        <Component {...pageProps} />
+      </div>
+    </SWRConfig>
   );
 }
