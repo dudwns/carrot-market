4bd0a58694e025281b230f4250594b9597cf973a
yeongjun Kim <kyjun519@gmail.com>
2023-04-11 21:29:19 +0900

    feat: product detail api 추가

---
+++ src/pages/api/products/[id]/index.ts
@@ -0,0 +1,63 @@
+import client from "@/libs/server/client";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+  const product = await client.product.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      user: {
+        // 유저 정보도 포함해서 가져옴
+        select: {
+          // id, name, avatar만 가져옴
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  const terms = product?.name.split(" ").map((word) => ({
+    name: {
+      contains: word,
+    },
+  }));
+  const relatedProducts = await client.product.findMany({
+    where: {
+      OR: terms,
+      AND: {
+        id: {
+          not: product?.id,
+        },
+      },
+    },
+  });
+  const isLiked = Boolean(
+    await client.fav.findFirst({
+      where: {
+        productId: product?.id,
+        userId: user?.id,
+      },
+      select: {
+        id: true,
+      },
+    })
+  );
+  res.json({ ok: true, product, isLiked, relatedProducts });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
+
+// withIronSessionApiRoute로 감싸면 req.session을 확인할 수 있다.
