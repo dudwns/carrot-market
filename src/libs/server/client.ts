b82ba4495899c9cf89bcb7171b3b936d59d4e905
yeongjun Kim <kyjun519@gmail.com>
2023-04-11 21:10:29 +0900

    feat: global client 추가

--- src/libs/server/client.ts
+++ src/libs/server/client.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
+declare global {
+  var client: PrismaClient | undefined;
+}
+
+const client = global.client || new PrismaClient(); // glbbal client가 없으면 객체 생성
+
+if (process.env.NODE_ENV === "development") global.client = client; // client를 global client에 넣음
+
 export default new PrismaClient();
