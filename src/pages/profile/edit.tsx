dd42a64737658d30d9cadbd05929093f64fff31c
yeongjun Kim <kyjun519@gmail.com>
2023-04-13 23:24:00 +0900

    feat: 프로필 수정 기능 추가

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -1,11 +1,62 @@
 import Button from "@/Components/button";
 import Input from "@/Components/input";
 import Layout from "@/Components/layout";
+import useMutation from "@/libs/client/useMutation";
+import useUser from "@/libs/client/useUser";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  name?: string;
+  formErrors?: string;
+}
+
+interface EditProfileResponse {
+  ok: boolean;
+  error?: string;
+}
 
 export default function Edit() {
+  const { user } = useUser();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+
+  const [editProfile, { data, loading }] = useMutation<EditProfileResponse>(`/api/users/me`);
+
+  const onValid = ({ email, phone, name }: EditProfileForm) => {
+    if (loading) return;
+    if (email == "" && phone == "" && name == "") {
+      return setError("formErrors", { message: "이름, 이메일, 전화번호 중 하나가 필요합니다." }); // 에러 설정
+    }
+    editProfile({
+      email,
+      phone,
+      name,
+    });
+  };
+
+  useEffect(() => {
+    if (user?.name) setValue("name", user.name);
+    if (user?.email) setValue("email", user.email); // form의 값을 바꿈
+    if (user?.phone) setValue("phone", user.phone);
+  }, [user, setValue]);
+
+  useEffect(() => {
+    if (data && !data.ok && data.error) {
+      setError("formErrors", { message: data.error });
+    }
+  }, [data, setError]);
+
   return (
     <Layout canGoBack>
-      <form className="py-10 px-4 space-y-4">
+      <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
           <div className="w-14 h-14 rounded-full bg-slate-500" />
           <label
@@ -16,10 +67,28 @@ export default function Edit() {
             <input id="picture" type="file" className="hidden" accept="image/*" />
           </label>
         </div>
-        <Input required label="Email address" name="email" type="email" />
-        <Input required label="Phone number" name="phone" type="number" kind="phone" />
-
-        <Button text="Update profile" />
+        <Input register={register("name")} required={false} label="Name" name="name" type="text" />
+        <Input
+          register={register("email")}
+          required={false}
+          label="Email address"
+          name="email"
+          type="email"
+        />
+        <Input
+          register={register("phone")}
+          required={false}
+          label="Phone number"
+          name="phone"
+          type="number"
+          kind="phone"
+        />
+        {errors.formErrors ? (
+          <span className="my-2 text-red-500 font-medium text-center block">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
+        <Button loading={loading} text="Update profile" />
       </form>
     </Layout>
   );
