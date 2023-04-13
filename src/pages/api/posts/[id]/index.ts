38f4a980ee0982636080b8d26c565c984df222e9
yeongjun Kim <kyjun519@gmail.com>
2023-04-14 04:37:23 +0900

    feat: Post Detail API 생성

---
+++ src/pages/api/posts/[id]/index.ts
@@ -0,0 +1,70 @@
+import client from "@/libs/server/client";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+  const post = await client.post.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          answer: true,
+          id: true,
+          createdAt: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wonderings: true,
+        },
+      },
+    },
+  });
+  const isWondering = Boolean(
+    await client.wondering.findFirst({
+      where: {
+        userId: user?.id,
+        postId: Number(id),
+      },
+      select: {
+        id: true,
+      },
+    })
+  );
+  res.json({
+    ok: true,
+    post,
+    isWondering,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
+
+// withIronSessionApiRoute로 감싸면 req.session을 확인할 수 있다.
