30873e42ec33864f0653b00f95a8770e951f22b5
yeongjun Kim <kyjun519@gmail.com>
2023-04-14 04:55:12 +0900

    feat: 실제 데이터로 변경

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -13,17 +13,16 @@ interface PostWithUser extends Post {
   };
 }
 
-interface PostResponse {
+interface PostsResponse {
   ok: boolean;
   posts: PostWithUser[];
 }
 
 export default function Comunity() {
   const { latitude, longitude } = useCoords();
-  const { data } = useSWR<PostResponse>(
+  const { data } = useSWR<PostsResponse>(
     latitude && longitude ? `/api/posts?latitude=${latitude}&longitude=${longitude}` : null
   );
-  console.log(latitude, longitude);
   return (
     <Layout title="커뮤니티" hasTabBar>
       <div className="space-y-4 divide-y-[2px]">
@@ -34,11 +33,12 @@ export default function Comunity() {
                 동네질문
               </span>
               <div className="mt-2 px-4 text-gray-700">
-                <span className="text-orange-500 font-medium">Q.</span> {post.question}
+                <span className="text-orange-500 font-medium">Q.</span>
+                {post.question}
               </div>
               <div className="mt-5 px-4 flex items-center justify-between w-full text-gray-500 font-medium text-xs">
                 <span>{post.user.name}</span>
-                <span>{String(post.createAt)}</span>
+                <span>{String(post.createdAt)}</span>
               </div>
               <div className="flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px] w-full">
                 <span className="flex space-x-2 items-center text-sm">
@@ -56,7 +56,7 @@ export default function Comunity() {
                       d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                     ></path>
                   </svg>
-                  <span>궁금해요 {post?._count?.wonderings}</span>
+                  <span>궁금해요 {post._count.wonderings}</span>
                 </span>
                 <span className="flex space-x-2 items-center text-sm">
                   <svg
@@ -73,7 +73,7 @@ export default function Comunity() {
                       d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                     ></path>
                   </svg>
-                  <span>답변 {post?._count?.answers}</span>
+                  <span>답변 {post._count.answers}</span>
                 </span>
               </div>
             </div>
