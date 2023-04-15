e0cecf0795025d59f917cd9b8fc53886fbeddfdc
yeongjun Kim <kyjun519@gmail.com>
2023-04-15 10:38:56 +0900

    feat: Stream Detail API 생성

---
+++ src/pages/api/streams/[id]/index.ts
@@ -0,0 +1,41 @@
+import client from "@/libs/server/client";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const {
+    query: { id },
+  } = req;
+  const stream = await client.stream.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      messages: {
+        select: {
+          id: true,
+          message: true,
+          user: {
+            select: {
+              avatar: true,
+              id: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  res.json({
+    ok: true,
+    stream,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
+// withIronSessionApiRoute로 감싸면 req.session을 확인할 수 있다.
