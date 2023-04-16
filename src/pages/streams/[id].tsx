3ce9557d888f09fe024cb6075d40b14bdf27e697
yeongjun Kim <kyjun519@gmail.com>
2023-04-16 19:23:43 +0900

    feat: 라이브 기능 추가

--- src/pages/streams/[id].tsx
+++ src/pages/streams/[id].tsx
@@ -70,11 +70,27 @@ export default function StreamDetail() {
   return (
     <Layout canGoBack>
       <div className="py-10 px-4 space-y-4">
-        <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+        <iframe
+          className="w-full aspect-video rounded-md shadow-sm"
+          src={`https://iframe.videodelivery.net/${data?.stream?.cloudflareId}`}
+          allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+          allowFullScreen={true}
+        ></iframe>
         <div className="mt-5">
           <h1 className="text-3xl font-bold text-gray-900">{data?.stream?.name}</h1>
           <span className="text-2xl block mt-3 text-gray-900">${data?.stream?.price}</span>
           <p className=" my-6 text-gray-700">{data?.stream?.description}</p>
+          <div className="bg-orange-400 flex flex-col space-y-3 p-5 rounded-md overflow-scroll">
+            <span>Stream Keys (secret)</span>
+            <span className="text-white">
+              <span className="font-meduim text-gray-800">URL: </span>
+              {data?.stream?.cloudflareUrl}
+            </span>
+            <span className="text-white">
+              <span className="font-meduim text-gray-800">Key: </span>
+              {data?.stream?.cloudflareKey}
+            </span>
+          </div>
         </div>
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Live Chat</h2>
