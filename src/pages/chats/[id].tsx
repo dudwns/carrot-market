31452e9b853f41161e0e40b0250527f28912c434
yeongjun Kim <kyjun519@gmail.com>
2023-04-18 08:04:31 +0900

    feat: 실제 데이터로 변경

--- src/pages/chats/[id].tsx
+++ src/pages/chats/[id].tsx
@@ -1,18 +1,70 @@
 import Layout from "@/Components/layout";
 import Message from "@/Components/message";
+import useMutation from "@/libs/client/useMutation";
+import useUser from "@/libs/client/useUser";
+import { Chat, ChatMessage, User } from "@prisma/client";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import useSWR from "swr";
+
+interface MessageForm {
+  message: string;
+}
+
+interface MessageWithChat extends ChatMessage {
+  chat: Chat;
+  user: User;
+}
+
+interface MessageResponse {
+  ok: boolean;
+  messages: MessageWithChat[];
+}
 
 export default function ChatDetail() {
+  const { user } = useUser();
+  const router = useRouter();
+  const [createdMessage, { data, loading }] = useMutation(`/api/chats/messages/${router.query.id}`);
+  const {
+    data: messageData,
+    isLoading,
+    mutate,
+  } = useSWR<MessageResponse>(`/api/chats/messages/${router.query.id}`);
+
+  const { register, handleSubmit, reset } = useForm<MessageForm>();
+  const onValid = ({ message }: MessageForm) => {
+    reset();
+    mutate(
+      (prev) =>
+        prev &&
+        ({
+          ...prev,
+          messages: [...prev.messages, { id: Date.now(), message: message, user: { ...user } }],
+        } as any),
+      false
+    );
+    createdMessage({ message });
+  };
+
   return (
     <Layout canGoBack>
       <div className="py-10 px-4 space-y-4">
-        <Message message="Hi how much are you selling them for?" />
-        <Message message="I want ￦20,000" reversed />
-        <Message message="미쳤어" />
-
-        <form className="fixed py-2 bg-white   bottom-0 inset-x-0 ">
+        {messageData?.messages?.map((message) => (
+          <Message
+            key={message?.id}
+            message={message?.message}
+            reversed={message?.user?.id === user?.id}
+            avatarUrl={message.user.avatar ? message.user.avatar : ""}
+          />
+        ))}
+        <form
+          onSubmit={handleSubmit(onValid)}
+          className="fixed py-2 bg-white   bottom-0 inset-x-0 "
+        >
           <div className="flex relative max-w-md items-center w-full mx-auto">
             <input
               type="text"
+              {...register("message")}
               className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
             />
             <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
