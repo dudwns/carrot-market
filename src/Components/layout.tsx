54f0154f7f886a149746410461280afe68264e1f
yeongjun Kim <kyjun519@gmail.com>
2023-04-18 07:56:49 +0900

    style: margin 수치 조정

--- src/Components/layout.tsx
+++ src/Components/layout.tsx
@@ -36,7 +36,7 @@ export default function Layout({ title, canGoBack, hasTabBar, children }: Layout
         ) : null}
         {title ? <span>{title}</span> : null}
       </div>
-      <div className={cls("pt-12", hasTabBar ? "pb-24" : "")}>{children}</div>
+      <div className={cls("pt-12", hasTabBar ? "pb-20" : "")}>{children}</div>
       {hasTabBar ? (
         <nav className="bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs">
           <Link href="/">
