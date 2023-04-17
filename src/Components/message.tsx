6bcac5497b8f469c5851a9e188054f3e40109438
yeongjun Kim <kyjun519@gmail.com>
2023-04-18 07:57:23 +0900

    feat: 채팅창 프로필 이미지 적용

--- src/Components/message.tsx
+++ src/Components/message.tsx
@@ -1,4 +1,5 @@
 import { cls } from "@/libs/client/utils";
+import Image from "next/image";
 
 interface MessageProps {
   message: string;
@@ -14,7 +15,18 @@ export default function Message({ message, avatarUrl, reversed }: MessageProps) 
         reversed ? "flex-row-reverse space-x-reverse" : ""
       )}
     >
-      <div className="w-8 h-8 rounded-full bg-slate-400" />
+      {avatarUrl ? (
+        <Image
+          alt="프로필 이미지"
+          width={100}
+          height={100}
+          src={avatarUrl}
+          className="w-8 h-8 rounded-full bg-slate-400"
+        />
+      ) : (
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+      )}
+
       <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
         <p>{message}</p>
       </div>
