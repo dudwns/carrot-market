431db1ee293a47751147c10ab9b73bc0d4f39a4e
yeongjun Kim <kyjun519@gmail.com>
2023-04-17 23:17:53 +0900

    feat: onClick method 추가

--- src/Components/button.tsx
+++ src/Components/button.tsx
@@ -10,6 +10,7 @@ interface ButtonProps {
 export default function Button({ large = false, onClick, loading, text, ...rest }: ButtonProps) {
   return (
     <button
+      onClick={onClick}
       {...rest}
       className={cls(
         "w-full bg-orange-500 hover:bg-orange-600 text-white  px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none",
