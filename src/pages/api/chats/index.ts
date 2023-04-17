692b001473495f4c5f875966b4e13be28306e6e3
yeongjun Kim <kyjun519@gmail.com>
2023-04-18 08:02:16 +0900

    feat: 채팅 리스트 API 추가

---
+++ src/pages/api/chats/index.ts
@@ -0,0 +1,71 @@
+import client from "@/libs/server/client";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const {
+    body: { productId, receivedId },
+    session: { user },
+  } = req;
+
+  if (req.method === "GET") {
+    const chats = await client.chat.findMany({
+      where: {
+        OR: [{ createdById: user?.id }, { createdForId: user?.id }],
+      },
+      include: {
+        createdFor: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+      },
+    });
+    res.json({ ok: true, chats });
+  }
+
+  if (req.method === "POST") {
+    const chatDuplicated = await client.chat.findFirst({
+      where: {
+        AND: [{ createdById: user?.id }, { createdForId: +receivedId }, { productId: +productId }],
+      },
+    });
+
+    if (!chatDuplicated) {
+      const chat = await client.chat.create({
+        data: {
+          createdBy: {
+            connect: {
+              id: user?.id,
+            },
+          },
+          createdFor: {
+            connect: {
+              id: +receivedId,
+            },
+          },
+          product: {
+            connect: {
+              id: +productId,
+            },
+          },
+        },
+      });
+      res.json({ ok: true, chat });
+    } else if (chatDuplicated) {
+      res.json({ ok: true, chat: chatDuplicated });
+    }
+  }
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET", "POST"],
+    handler,
+  })
+);
+
+// withIronSessionApiRoute로 감싸면 req.session을 확인할 수 있다.
