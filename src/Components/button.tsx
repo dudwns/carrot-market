64054a4bd213e51a08370b2622e5a70d42adcf20
yeongjun Kim <kyjun519@gmail.com>
2023-04-19 10:58:16 +0900

    feat: isSale prop 추가

--- src/Components/button.tsx
+++ src/Components/button.tsx
@@ -1,20 +1,29 @@
 import { cls } from "@/libs/client/utils";
 
 interface ButtonProps {
+  isSale?: boolean;
   large?: boolean;
   text: string;
   loading?: boolean;
   [key: string]: any;
 }
 
-export default function Button({ large = false, onClick, loading, text, ...rest }: ButtonProps) {
+export default function Button({
+  isSale,
+  large = false,
+  onClick,
+  loading,
+  text,
+  ...rest
+}: ButtonProps) {
   return (
     <button
       onClick={onClick}
       {...rest}
       className={cls(
         "w-full bg-orange-500 hover:bg-orange-600 text-white  px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none",
-        large ? "py-3 text-base" : "py-2 text-sm "
+        large ? "py-3 text-base" : "py-2 text-sm ",
+        isSale ? "pointer-events-none" : ""
       )}
     >
       {loading ? "Loading..." : text}
