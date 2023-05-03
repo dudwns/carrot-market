f604c59d705376b1566aec0b332180e5a81c1e32
yeongjun Kim <kyjun519@gmail.com>
2023-05-03 23:49:38 +0900

    feat: Middleware 적용

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextFetchEvent, userAgent } from "next/server";
+import { NextResponse } from "next/server";
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  //   console.log("it works! global middleware"); // global 적용
+
+  if (req.nextUrl.pathname.startsWith("/")) {
+    const ua = userAgent(req);
+    if (ua.isBot) {
+      // bot일 때 접근 제한
+      return new Response("봇은 이용할 수 없어요!", { status: 403 });
+    }
+  }
+  if (req.nextUrl.pathname.startsWith("/chats")) {
+    // chats 하위 폴더에서만 미들웨어 적용
+    console.log("chats ONLY middleware");
+  }
+  if (!req.url.includes("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.get("carrotsession")) {
+      NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+    }
+  }
+
+  return NextResponse.next();
+}
