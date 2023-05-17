d170140b2786a6b69baffb53eeb12e264bdd2def
yeongjun Kim <kyjun519@gmail.com>
2023-05-17 16:30:44 +0900

    fix: 미들웨어 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,7 +16,7 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
   }
   if (!req.url.includes("/api")) {
     if (!req.url.includes("/enter") && !req.cookies.get("carrotsession")) {
-      NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+      return NextResponse.redirect(new URL("/enter", req.url));
     }
   }
 
